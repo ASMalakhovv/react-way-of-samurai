470471e9b9b97d871c40fb43dac0f15c68cceb56
ASMalakhovv <alsmalahovv@mail.ru>
2021-11-03 18:17:42 +0600

    created a global store object

--- src/Redux/State.ts
+++ src/Redux/State.ts
@@ -38,98 +38,107 @@ export type StateType = {
     sidebar: SideBarType
 }
 
-
-let onChange = (state:StateType) => {
-    console.log('state changed')
+export type StoreType = {
+    _state: StateType
+    addPosts: () => void
+    updateNewPostText: (changeValue: string) => void
+    onChange: (store: StoreType) => void
 }
 
-export const state: StateType = {
-    dialogsPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: "Andrew",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSR4Cpu6bJCTK6-7ffqpAS-kojHGxKMiJPw&usqp=CAU"
-            },
-            {
-                id: 2,
-                name: "Dmitry",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoHS8XI7wztGL0dfquA9UIPjW9PDKCPZ2wA&usqp=CAU"
-            },
-            {
-                id: 3,
-                name: "Sasha",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfxi6qu8OUSnLlE1BQhdTQn8M5eJMs9sNRvA&usqp=CAU"
-            },
-            {
-                id: 4,
-                name: "Sveta",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI6jRNS2_D9zbiR1EoAcKUE6goQWy3GlFJYQ&usqp=CAU"
-            },
-            {
-                id: 5,
-                name: "Valera",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhkWSkQV9ae0HZ_yOHmCZ7ZD1nnu-LqMP9vA&usqp=CAU"
-            },
-            {
-                id: 6,
-                name: "Viktor",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQGkx_9jSYDff6SAGbW670JIvNGwq40XDIQ&usqp=CAU"
-            },
-            {
-                id: 7,
-                name: "Marina",
-                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPwJzIsM4w_isvSQchgA1JnY4Vrpl_uyq4Q&usqp=CAU"
-            }
-        ],
-        messages: [
-            {id: 1, message: "Hi"},
-            {id: 2, message: "How are you"},
-            {id: 3, message: "YO"},
-            {id: 4, message: "YO"},
-            {id: 5, message: "YO"},
-            {id: 6, message: "YO"},
-            {id: 7, message: "YO"},
-        ],
-    },
-    profilePage: {
-        newPostText: "",
-        posts: [
-            {id: 1, message: "Hi, how are you", like: 15},
-            {id: 2, message: "It's my first post", like: 10}
-        ],
-        addressImage: "https://assets.bellator.com/andrey_koreshkov_1990/08/23_banner/original-1619507089.jpg"
-    },
-    sidebar: {
-        title: [
-            {id: "/profile", item: "Profile"},
-            {id: "/message", item: "Messages"},
-            {id: "/news", item: "News"},
-            {id: "/music", item: "Music"},
-            {id: "/settings", item: "Settings"},
+let store: StoreType = {
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: "Andrew",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSR4Cpu6bJCTK6-7ffqpAS-kojHGxKMiJPw&usqp=CAU"
+                },
+                {
+                    id: 2,
+                    name: "Dmitry",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoHS8XI7wztGL0dfquA9UIPjW9PDKCPZ2wA&usqp=CAU"
+                },
+                {
+                    id: 3,
+                    name: "Sasha",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfxi6qu8OUSnLlE1BQhdTQn8M5eJMs9sNRvA&usqp=CAU"
+                },
+                {
+                    id: 4,
+                    name: "Sveta",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI6jRNS2_D9zbiR1EoAcKUE6goQWy3GlFJYQ&usqp=CAU"
+                },
+                {
+                    id: 5,
+                    name: "Valera",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhkWSkQV9ae0HZ_yOHmCZ7ZD1nnu-LqMP9vA&usqp=CAU"
+                },
+                {
+                    id: 6,
+                    name: "Viktor",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQGkx_9jSYDff6SAGbW670JIvNGwq40XDIQ&usqp=CAU"
+                },
+                {
+                    id: 7,
+                    name: "Marina",
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPwJzIsM4w_isvSQchgA1JnY4Vrpl_uyq4Q&usqp=CAU"
+                }
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How are you"},
+                {id: 3, message: "YO"},
+                {id: 4, message: "YO"},
+                {id: 5, message: "YO"},
+                {id: 6, message: "YO"},
+                {id: 7, message: "YO"},
+            ],
+        },
+        profilePage: {
+            newPostText: "",
+            posts: [
+                {id: 1, message: "Hi, how are you", like: 15},
+                {id: 2, message: "It's my first post", like: 10}
+            ],
+            addressImage: "https://assets.bellator.com/andrey_koreshkov_1990/08/23_banner/original-1619507089.jpg"
+        },
+        sidebar: {
+            title: [
+                {id: "/profile", item: "Profile"},
+                {id: "/message", item: "Messages"},
+                {id: "/news", item: "News"},
+                {id: "/music", item: "Music"},
+                {id: "/settings", item: "Settings"},
 
-        ],
-        additionally: "Friends"
+            ],
+            additionally: "Friends"
+        }
+    },
+    addPosts() {
+        const post: PostsTypeObject = {id: 3, message: store._state.profilePage.newPostText, like: 0};
+        store._state.profilePage.posts.push(post);
+        store._state.profilePage.newPostText = ""
+        this.onChange(store)
+    },
+    updateNewPostText(changeValue) {
+        store._state.profilePage.newPostText = changeValue
+        this.onChange(store)
+    },
+    onChange(store) {
+        console.log('store changed')
     }
+
 }
 
-export default state;
+let
 
-export const addPosts = () => {
-    const post: PostsTypeObject = {id: 3, message: state.profilePage.newPostText, like: 0};
-    state.profilePage.posts.push(post);
-    state.profilePage.newPostText = ""
-    onChange(state)
-};
+    export
+default
+store;
 
 
-export const updateNewPostText = (changeValue: string) => {
-    state.profilePage.newPostText = changeValue
-    onChange(state)
-}
-
-
-export const subscribe = (callback: (state:StateType) => void) => {
+export const subscribe = (callback: (state: StateType) => void) => {
     onChange = callback
 }
 
